e8fe2f039a2f3657830e75d51aba551de13228a9
changrong.zeng <changrong.zeng@oyohotels.cn>
2019-12-14 16:37:35 +0800

    初始化

---
+++ src/main/java/com/domain/springboottool/SpringBootToolApplication.java
@@ -0,0 +1,13 @@
+package com.domain.springboottool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domain/springboottool/SpringBootToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.springboottool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
